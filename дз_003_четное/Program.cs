cf192b4aff370b6406ea93ca7f25dd54c412b377
NATALI <nata_matvienk@mail.ru>
2022-02-06 18:35:52 +0200

    четное

---
+++ дз_003_четное/Program.cs
@@ -0,0 +1,9 @@
+﻿// является ли число четным
+
+int number = new Random().Next(1,1000);
+Console.WriteLine(number);
+int ostatok = number / 2;
+if(ostatok == 0)
+Console.WriteLine("четное");
+else
+Console.WriteLine("нечетное");
