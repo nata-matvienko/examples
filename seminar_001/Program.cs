826592573bf9cefcb50d7156b59b3cff895cf386
NATALI <nata_matvienk@mail.ru>
2022-02-06 16:10:26 +0200

    квадрат числа

---
+++ seminar_001/Program.cs
@@ -0,0 +1,10 @@
+﻿// вывести квадрат числа
+
+//выводим на консоль сообщение
+Console.Write("Введите число->");
+
+string numberStr = Console.Readline();
+
+int number = int.Parse(numberStr);
+
+Console.WriteLine($"квадрат числа: {number * number}");
