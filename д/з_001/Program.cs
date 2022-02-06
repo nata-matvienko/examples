ba4a4ecb08d4bf4e15e57d5f3600af10d14c5503
NATALI <nata_matvienk@mail.ru>
2022-02-06 18:09:36 +0200

    наибольшее и наименьшее из 2х чисел

---
+++ д/з_001/Program.cs
@@ -0,0 +1,7 @@
+﻿// показать большее и меньшее число из 2х
+
+int numberA = 9;
+int numberB = 7;
+int min = Math.Min(numberA, numberB);
+int max = Math.Max(numberA, numberB);
+Console.WriteLine(string.Format("min:{0} max:{1}", min, max));
