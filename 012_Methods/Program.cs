7830bd34be877b683d59a89b1ab84c51cc5c06a2
NATALI <nata_matvienk@mail.ru>
2022-02-08 17:03:13 +0200

    виды функций и цикл for

--- 012_Methods/Program.cs
+++ 012_Methods/Program.cs
@@ -1,4 +1,4 @@
-﻿// вид 1
+﻿/*// вид 1
 void Method1()
 {
     Console.WriteLine("Автор ...");
@@ -51,5 +51,28 @@ string Method4(int count, string text)
     return result;
 }
 string res = Method4(10, "asdf");
-Console.WriteLine(res);
+//Console.WriteLine(res);*/
 
+// Вид 4 применяя цикл for
+string Method41(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result +text;       
+    }
+    return result;
+}
+string res = Method41(10, "z");
+//Console.WriteLine(res);
+
+
+
+for (int i = 2; i<=10; i++)
+{
+    for (int j = 2; j<=10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
